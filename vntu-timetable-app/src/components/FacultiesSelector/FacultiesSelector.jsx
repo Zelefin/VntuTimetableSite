5562f65fd3f81b1fa78648516b18089717ba7fe2
Zelefin <nagangoroh@gmail.com>
2024-04-11 21:50:35 +0300

    added max width

--- vntu-timetable-app/src/components/FacultiesSelector/FacultiesSelector.jsx
+++ vntu-timetable-app/src/components/FacultiesSelector/FacultiesSelector.jsx
@@ -4,7 +4,7 @@ function FacultiesSelector({ faculties, facultyId, handleFacultyChange }) {
       <label className="text-white inline-flex w-full items-center">
         Факультет:
         <select
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 max-w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={facultyId}
           onChange={handleFacultyChange}
         >
