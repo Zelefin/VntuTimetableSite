965740865a2c4865af1a5957617907482cf05530
Zelefin <nagangoroh@gmail.com>
2024-04-24 11:04:39 +0300

    navigating instead of changing link

--- vntu-timetable-app/src/pages/RootPage.jsx
+++ vntu-timetable-app/src/pages/RootPage.jsx
@@ -7,28 +7,26 @@ function RootPage() {
 
   useEffect(() => {
     if (searchParams.get("tgWebAppStartParam")) {
-      const tgparams = searchParams.get("tgWebAppStartParam").split("_");
-      {
-        /* facultyId_groupId */
-      }
+      const tgparams = searchParams.get("tgWebAppStartParam").split("_"); // facultyId_groupId
       const facultyParam = parseInt(tgparams[0]);
       const groupParam = parseInt(tgparams[1]);
       navigate(`/${facultyParam}/${groupParam}`);
     }
-  });
+  }, []);
+
+  const handleLinkClick = () => {
+    navigate(`/210/10840`);
+  };
 
   return (
-    <div className="bg-gray-900 overflow-auto w-full h-full text-white font-bold">
+    <div className="text-white font-bold">
       <h1 className="m-5">
         Вітаю! Це root сторінка. Взагалі це не повинно бути видно, тому якщо Ви
         очікували побачити розклад, то це точно баг... Щоб переглянути розклад
         можна перейти по посиланню:
       </h1>
       <div className="m-5">
-        <a
-          href="VntuTimetableSite/210/10840"
-          className="text-blue-400 break-words"
-        >
+        <a onClick={handleLinkClick} className="text-blue-400 break-words">
           https://zelefin.github.io/VntuTimetableSite/210/10840
         </a>
       </div>
